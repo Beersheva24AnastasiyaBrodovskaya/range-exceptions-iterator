8d832ab10838603655687427ada1ee5d3a125a67
Anastasiya Brodovskaya <leviyash@gmail.com>
2024-08-16 20:07:52 +0300

    HW18

--- src/main/java/telran/range/Range.java
+++ src/main/java/telran/range/Range.java
@@ -1,28 +1,72 @@
 package telran.range;
 
+import java.util.Iterator;
+import java.util.function.Predicate;
+
 import telran.range.exceptions.OutOfRangeMaxValueException;
 import telran.range.exceptions.OutOfRangeMinValueException;
 
-public class Range {
+public class Range implements Iterable<Integer> {
+
     private static final String ERROR_MESSAGE = "max less or equal min";
     private int min;
     private int max;
+    private Predicate<Integer> predicate;
+
     private Range(int min, int max) {
         this.min = min;
         this.max = max;
     }
+
     public static Range getRange(int min, int max) {
         if (max <= min) {
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
         return new Range(min, max);
     }
+
+    void setPredicate(Predicate<Integer> predicate) {
+        this.predicate = predicate;
+    }
+
     public void checkNumber(int number) throws OutOfRangeMaxValueException, OutOfRangeMinValueException {
-        if(number > max) {
+        if (number > max) {
             throw new OutOfRangeMaxValueException(max, number);
         }
         if (number < min) {
             throw new OutOfRangeMinValueException(min, number);
         }
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new RangeIterator();
+    }
+
+    private class RangeIterator implements Iterator<Integer> {
+
+        int current = min;
+        Predicate<Integer> localPredicate = predicate != null ? predicate : x -> true;
+
+        @Override
+        public boolean hasNext() {
+            //TODO
+
+            return current <= max;
+        }
+
+        @Override
+        public Integer next() {
+            //TODO
+            int res;
+            while (!localPredicate.test(current)) {
+                current++;
+            }
+            res = current;
+            current++;
+            return res;
+
+        }
+
     }
 }
--- src/test/java/telran/range/RangeTest.java
+++ src/test/java/telran/range/RangeTest.java
@@ -1,27 +1,65 @@
 package telran.range;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Iterator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
 
 import telran.range.exceptions.OutOfRangeMaxValueException;
 import telran.range.exceptions.OutOfRangeMinValueException;
 
 public class RangeTest {
+
     private static final int MIN = 50;
     private static final int MAX = 100;
     Range range = Range.getRange(MIN, MAX);
+
     @Test
     void wrongRangeCtreatingTest() {
-        assertThrowsExactly(IllegalArgumentException.class, () -> Range.getRange(MAX, MIN) );
+        assertThrowsExactly(IllegalArgumentException.class, () -> Range.getRange(MAX, MIN));
     }
+
     @Test
     void rightNumberTest() throws Exception {
         range.checkNumber(55);
     }
+
     void wrongNumberTest() throws Exception {
         assertThrowsExactly(OutOfRangeMaxValueException.class,
-         () -> range.checkNumber(MAX + 1));
-         assertThrowsExactly(OutOfRangeMinValueException.class, () -> range.checkNumber(MIN - 1));
+                () -> range.checkNumber(MAX + 1));
+        assertThrowsExactly(OutOfRangeMinValueException.class, () -> range.checkNumber(MIN - 1));
     }
+
+    @Test
+    void iteratorTest() {
+        //TODO
+        //different test cases
+        Iterator<Integer> iterator = range.iterator();
+        var a = MIN;
+        while (iterator.hasNext()) {
+            // assertTrue(a<=MAX);
+            if (a > MAX) {
+                fail();
+            }
+            assertEquals(a++, iterator.next());
+        }
+        range.setPredicate(x -> x % 2 == 0);
+        if (MIN % 2 == 0) {
+            a = MIN;
+        } else {
+            a = MIN + 1;
+        }
+
+        for (Integer x : range) {
+            if (a > MAX) {
+                fail();
+            }
+            assertEquals(a, x);
+            a += 2;
+        }
+
+    }
+
 }
