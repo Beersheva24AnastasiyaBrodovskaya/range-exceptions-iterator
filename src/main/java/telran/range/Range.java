e5144878879b81472b7e9e6d8001af64e6d97b21
Anastasiya Brodovskaya <leviyash@gmail.com>
2024-08-22 08:59:47 +0300

    CW18

--- src/main/java/telran/range/Range.java
+++ src/main/java/telran/range/Range.java
@@ -7,69 +7,64 @@ import java.util.function.Predicate;
 import telran.range.exceptions.OutOfRangeMaxValueException;
 import telran.range.exceptions.OutOfRangeMinValueException;
 
-public class Range implements Iterable<Integer> {
-
+public class Range implements Iterable<Integer>{
     private static final String ERROR_MESSAGE = "max less or equal min";
     private int min;
     private int max;
-    private Predicate<Integer> predicate;
-
+    private Predicate<Integer> predicate = n -> true;
     private Range(int min, int max) {
         this.min = min;
-        this.max = max;
+        this.max = max; 
     }
-
     public static Range getRange(int min, int max) {
         if (max <= min) {
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
         return new Range(min, max);
     }
-
     void setPredicate(Predicate<Integer> predicate) {
         this.predicate = predicate;
     }
-
     public void checkNumber(int number) throws OutOfRangeMaxValueException, OutOfRangeMinValueException {
-        if (number > max) {
+        if(number > max) {
             throw new OutOfRangeMaxValueException(max, number);
         }
         if (number < min) {
             throw new OutOfRangeMinValueException(min, number);
         }
     }
-
     @Override
     public Iterator<Integer> iterator() {
         return new RangeIterator();
     }
-
     private class RangeIterator implements Iterator<Integer> {
-
-        int current = min;
-        Predicate<Integer> localPredicate = predicate != null ? predicate : x -> true;
-
+        int current = min - 1;
         @Override
         public boolean hasNext() {
-            //TODO
-
+           
             return current <= max;
+        }
+        public RangeIterator() {
+            setNextCurrent();
         }
 
         @Override
         public Integer next() {
-            //TODO
-            if (!hasNext()) {
+            if(!hasNext()) {
                 throw new NoSuchElementException();
             }
-            int res;
-            while (!localPredicate.test(current) && (current <= max)) {
-                current++;
-            }
-
-            return current++;
-
+            int toBeIterated = current;
+            setNextCurrent();
+            return toBeIterated;
         }
 
+        private void setNextCurrent() {
+            current++;
+            while(current <= max && !predicate.test(current)) {
+                current++;
+            }
+            
+        }
+        
     }
 }
--- src/test/java/telran/range/RangeTest.java
+++ src/test/java/telran/range/RangeTest.java
@@ -1,65 +1,60 @@
 package telran.range;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
-import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
 
 import telran.range.exceptions.OutOfRangeMaxValueException;
 import telran.range.exceptions.OutOfRangeMinValueException;
 
 public class RangeTest {
-
     private static final int MIN = 50;
     private static final int MAX = 100;
     Range range = Range.getRange(MIN, MAX);
-
     @Test
     void wrongRangeCtreatingTest() {
-        assertThrowsExactly(IllegalArgumentException.class, () -> Range.getRange(MAX, MIN));
+        assertThrowsExactly(IllegalArgumentException.class, () -> Range.getRange(MAX, MIN) );
     }
-
     @Test
     void rightNumberTest() throws Exception {
         range.checkNumber(55);
     }
-
     void wrongNumberTest() throws Exception {
         assertThrowsExactly(OutOfRangeMaxValueException.class,
-                () -> range.checkNumber(MAX + 1));
-        assertThrowsExactly(OutOfRangeMinValueException.class, () -> range.checkNumber(MIN - 1));
+         () -> range.checkNumber(MAX + 1));
+         assertThrowsExactly(OutOfRangeMinValueException.class, () -> range.checkNumber(MIN - 1));
     }
-
     @Test
     void iteratorTest() {
-        //TODO
-        //different test cases
-        Iterator<Integer> iterator = range.iterator();
-        var a = MIN;
-        while (iterator.hasNext()) {
-            // assertTrue(a<=MAX);
-            if (a > MAX) {
-                fail();
-            }
-            assertEquals(a++, iterator.next());
-        }
-        range.setPredicate(x -> x % 2 == 0);
-        if (MIN % 2 == 0) {
-            a = MIN;
-        } else {
-            a = MIN + 1;
-        }
-
-        for (Integer x : range) {
-            if (a > MAX) {
-                fail();
-            }
-            assertEquals(a, x);
-            a += 2;
-        }
+        Range range = Range.getRange(1, 3);
+        runTest(new Integer[] {1,2,3}, range);
+    }
+    @Test
+    void iteratorPredicateMatchTest() {
+        Range range = Range.getRange(1, 3);
+        range.setPredicate(n -> n % 2 == 0); 
+        runTest(new Integer[]{2}, range);
+        range.setPredicate(n -> n % 2 != 0); 
+        runTest(new Integer[]{1, 3}, range);
 
     }
+    @Test
+    void iteratorPredicateNoMatchTest() {
+        range.setPredicate(n -> n > MAX);
+        runTest(new Integer[0], range);
 
+    }
+    private void runTest(Integer[] expected, Range range) {
+        Integer [] actual = new Integer[expected.length];
+        Iterator<Integer> it = range.iterator();
+        int index = 0;
+        while(it.hasNext()) {
+            actual[index++] = it.next();
+        }
+        assertArrayEquals(expected, actual);
+        assertThrowsExactly(NoSuchElementException.class, it::next);
+    }
 }
